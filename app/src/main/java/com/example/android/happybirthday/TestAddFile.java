63efbeb976cc5b64a74bcd46c1cc75896039da57
Henning Ohlenholz <h.ohlenholz@outlook.de>
2016-03-01 13:13:52 +0100

    Change in TestAddFile, Change font size

--- app/src/main/java/com/example/android/happybirthday/TestAddFile.java
+++ app/src/main/java/com/example/android/happybirthday/TestAddFile.java
@@ -4,4 +4,8 @@ package com.example.android.happybirthday;
  * Created by Henning Ohlenholz on 01.03.2016.
  */
 public class TestAddFile {
+
+    private void TestReverse(){
+        int c = 13;
+    }
 }
